1932e0eb36ce40d9d37eef24949285643efb5f2d
Lev Alyukov <lev.alyukov@gmail.com>
2024-09-10 21:03:38 +0500

    v0.4
    - Added new command `/tomorrow`

--- app.py
+++ app.py
@@ -59,10 +59,36 @@ async def schedule(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     else:
         await update.message.reply_text("Пожалуйста, укажите учебную группу после команды. Пример: `/schedule 112`", parse_mode=ParseMode.MARKDOWN)
 
+async def tomorrow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.effective_user.id
+    if not await check_cooldown(user_id):
+        await update.message.reply_text("Подождите немного перед повторным использованием этой команды.")
+        return
+    
+    if context.args:
+        number_value = context.args[0]
+        logger.info(f"The '/schedule {number_value}' command was used")
+        if number_value in config['groups']:
+            try:
+                logger.info("Fetching schedule...")
+                start_time = time.time()
+                schedule_result = await get_schedule_tomorrow(number_value)
+                end_time = time.time()
+                logger.info(f"Schedule fetched in {end_time - start_time} seconds")
+
+                await update.message.reply_text(schedule_result, parse_mode=ParseMode.MARKDOWN)
+            except Exception as e:
+                logger.error(f"Error fetching schedule: {e}")
+        else:
+            await update.message.reply_text("Указать можно только доступные учебные группы: " + ", ".join(config['groups']))
+    else:
+        await update.message.reply_text("Пожалуйста, укажите учебную группу после команды. Пример: `/tomorrow 112`", parse_mode=ParseMode.MARKDOWN)
+
 if __name__ == "__main__":
     app = ApplicationBuilder().token(config["token"]).build()
     logger.info("Bot is ready to work")
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help))
     app.add_handler(CommandHandler("schedule", schedule))
+    app.add_handler(CommandHandler("tomorrow", tomorrow))
     app.run_polling()
